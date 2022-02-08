86302508988daab8d7b0a3e4abe418925679465d
mohamedelhafy <40215551+mohamedelhafy@users.noreply.github.com>
2022-02-08 15:20:06 +0200

    Add files via upload

--- 02-MCAL/01-RCC/RCC_config.h
+++ 02-MCAL/01-RCC/RCC_config.h
@@ -8,27 +8,26 @@
 #ifndef RCC_CONFIG_H
 #define RCC_CONFIG_H
 
-	/* Options : 	RCC_HSE_CRYSTAL
-					RCC_HSE_RC
-					RCC_HSI
-					RCC_PLL               */
-	#define RCC_CLOCK_TYPE		RCC_HSI
+/* Options :    RCC_HSE_CRYSTAL
+                RCC_HSE_RC
+                RCC_HSI
+                RCC_PLL               */
+#define RCC_HSICLOCK_TYPE		RCC_HSI
 
 
-	/*  Incase you choose the PLL option
+/*  Incase you choose the PLL option
 
-		Options :	RCC_PLL_IN_HSIDiv2
-					RCC_PLL_IN_HSEDiv2
-					RCC_PLL_IN_HSE
-	*/
-	/*  Note    :   Select Value only if you you have PLL as input Clock source */
-	#if 	RCC_CLOCK_TYPE == RCC_PLL
-	#define RCC_PLL_INPUT		RCC_PLL_IN_HSIDiv2
+    Options :   RCC_PLL_IN_HSIDiv2
+                RCC_PLL_IN_HSEDiv2
+                RCC_PLL_IN_HSE
+*/
+/*  Note    :   Select Value only if you you have PLL as input Clock source */
 
-	// Choose the Mul Value
-	/*	Options :   from 2 to 16 */
-	#define	RCC_PULL_MUL_VAL	9// choosing 9 as default val Feel free to chang it accordig your ablication	    
-	#endif
+#define RCC_PLL_INPUT          RCC_PLL_IN_HSIDiv2
+
+// Choose the Mul Value
+/*	Options :   from 2 to 16 */
+#define	RCC_PULL_MUL_VAL       9// choosing 9 as default val Feel free to chang it accordig your ablication
 
 
 
