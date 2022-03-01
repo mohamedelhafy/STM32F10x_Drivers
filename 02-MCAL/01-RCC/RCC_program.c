6422fc7c30e141d68da4c4e5422d822ccafc9a45
Mohamed Elhafy <mohamedelhafy2017@gmail.com>
2022-03-01 23:24:38 +0200

    rwsd

--- 02-MCAL/01-RCC/RCC_program.c
+++ 02-MCAL/01-RCC/RCC_program.c
@@ -27,7 +27,81 @@
 /********************************************************************************************************/
 void RCC_vInitSysClock(void)
 {
-	
+	#if 	RCC_CLOCK_TYPE == RCC_HSE_CRYSTAL
+
+		SET_BIT(RCC_CR , RCC_HSEON);				// Enable the HSE 
+		while(IS_BIT_CLEAR(RCC_CR ,RCC_HSERDY));	// Wait till the Hardware set the ready bit
+		CLR_BIT(RCC_CR , RCC_HSEPYB);				// Make sure to clear the ByPass Bit 
+		SET_BIT(RCC_CR , RCC_CSSON);				// Enable the Clock security System circuit
+		SET_BIT(RCC_CFGR, RCC_SW0);
+
+	#elif	RCC_CLOCK_TYPE == RCC_HSE_RC 
+
+		SET_BIT(RCC_CR , RCC_HSEON);				// Enable The HSE
+		while(IS_BIT_CLEAR(RCC_CR ,RCC_HSERDY));	// Wait till the Hardware set the ready bit
+		SET_BIT(RCC_CR , RCC_HSEPYB);				// Set ByPass to RC circuit
+		SET_BIT(RCC_CR , RCC_CSSON);				// Enable the Clock security System circuit
+		SET_BIT(RCC_CFGR, RCC_SW0);
+		CLR_BIT(RCC_CFGR, RCC_SW1);
+
+	#elif	RCC_CLOCK_TYPE == RCC_HSI 
+
+		SET_BIT(RCC_CR , RCC_HSION);                // Enable the HSI
+		while(IS_BIT_CLEAR(RCC_CR ,RCC_HSIRDY));    // Wait till the Hardware set the ready bit
+		SET_BIT(RCC_CR , RCC_CSSON);                // Enable the Clock security System circuit
+		// Choose the Input Clock to the processor
+		CLR_BIT(RCC_CFGR, RCC_SW0);
+		CLR_BIT(RCC_CFGR, RCC_SW1);
+
+	#elif	RCC_CLOCK_TYPE == RCC_PLL
+
+		#if 	RCC_PLL_INPUT == RCC_PLL_IN_HSIDiv2
+			
+			SET_BIT(RCC_CR, RCC_HSION);                  // Enable the HSI
+			while(IS_BIT_CLEAR(RCC_CR, RCC_HSIRDY));     // Wait till the Hardware set the ready bit  
+			CLR_BIT(RCC_CFGR, RCC_PLLSRC);               // Select the PLL Source as HSI/2
+			SET_BIT(RCC_CR, RCC_PLLON);                  // Enable the PLL 
+			while(IS_BIT_CLEAR(RCC_CR, RCC_PLLRDY));     // Wait till the Hardware set the ready bit 
+			// Choose the Input Clock to the processor
+			SET_BIT(RCC_CFGR, RCC_SW1);
+			CLR_BIT(RCC_CFGR, RCC_SW0);
+
+		#elif	RCC_PLL_INPUT == RCC_PLL_IN_HSEDiv2
+
+			SET_BIT(RCC_CR, RCC_HSEON);                  // Enable the HSE   
+			while(IS_BIT_CLEAR(RCC_CR, RCC_HSERDY));     // Wait till the Hardware set the ready bit
+			SET_BIT(RCC_CFGR, RCC_PLLSRC);               // Select the PLL Source as HSE
+			SET_BIT(RCC_CFGR, RCC_PLLXTPRE);             // Select the Pre Scaler
+			SET_BIT(RCC_CR, RCC_PLLON);                  // Enable the PLL      
+			while(IS_BIT_CLEAR(RCC_CR, RCC_PLLRDY));     // Wait till the Hardware set the ready bit 
+			// Choose the Input Clock to the processor               
+			SET_BIT(RCC_CFGR, RCC_SW1);
+			CLR_BIT(RCC_CFGR, RCC_SW0);
+
+		#elif	RCC_PLL_INPUT == RCC_PLL_IN_HSE
+
+			SET_BIT(RCC_CR, RCC_HSEON);                 // Enable the HSE   
+			while(IS_BIT_CLEAR(RCC_CR, RCC_HSERDY));    // Wait till the Hardware set the ready bit    
+			SET_BIT(RCC_CFGR, RCC_PLLSRC);              // Select the PLL Source as HSE
+			CLR_BIT(RCC_CFGR, RCC_PLLXTPRE);            // Select the Pre Scaler
+			SET_BIT(RCC_CR, RCC_PLLON);                 // Enable the PLL      
+			while(IS_BIT_CLEAR(RCC_CR, RCC_PLLRDY));    // Wait till the Hardware set the ready bit 
+			// Choose the Input Clock to the processor 
+			SET_BIT(RCC_CFGR, RCC_SW1);
+			CLR_BIT(RCC_CFGR, RCC_SW0);
+
+		#else	
+			#error ("Please Choose the Right PLL Option") 
+		#endif
+		
+	#else     
+		#error ("you Choose wrong Clock Type")
+	#endif
+
+
+// Handling the PLL MUL 
+// Handling the Bus Pre 
+}
 
 
 
