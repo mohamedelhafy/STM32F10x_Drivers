2242148b56a4d48ee81059eea763cd449286bbd9
Mohamed Elhafy <mohamedelhafy2017@gmail.com>
2022-03-12 20:52:04 +0200

    DMA && MY RTOS && EXTI && IRNEC

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -33,4 +33,28 @@ typedef long double   float96;    // New name for long double as float96
 
 
 
+
+// other typeDefs
+
+typedef unsigned char   u8;             // New name for unsigned char as u8
+typedef signed   char   s8;            // New name for signed char as s8
+
+typedef unsigned short  u16;            // New name for unsigned short as u16
+typedef signed   short  s16;           // New name for signed short as s16
+
+typedef unsigned long  u32;             // New name for unsigned long as u32
+typedef signed   long  s32;            // New name for signed long as s32
+
+typedef unsigned long long  u64;        // New name for unsigned long long as u64
+typedef signed   long long  s64;       // New name for signed long long as s64 
+
+
+typedef float         f32;          // New name for float as f32
+typedef double        f64;         // New name for double as f64
+typedef long double   f96;        // New name for long double as f96
+
+
+
+
+
 #endif // STD_TYPES_H
--- 02-MCAL/04-EXTI/EXTI_program.c
+++ 02-MCAL/04-EXTI/EXTI_program.c
@@ -21,7 +21,7 @@
 #include "EXTI_config.h"
 #include "EXTI_private.h"
 
-
+void (* EXTI_fCallBack[10])(void);
 
 
 
@@ -33,7 +33,7 @@
 /*                         void (* copyEXTI_fCallBake)(void)                                            */
 /*	Function Description : Enable Interupt And srt the Call Back                                        */
 /********************************************************************************************************/
-void EXTI_vEnableEXTI(E_Line_t copy_E_Line, E_Port_t copy_E_Port, E_State_t copy_E_Mode, void (* copyEXTI_fCallBake)(void))
+void EXTI_vEnableEXTI(E_Line_t copy_E_Line, E_Port_t copy_E_Port, E_State_t copy_E_Mode, void (* copyEXTI_fCallBack)(void))
 {
 	switch(copy_E_Mode)
 	{
@@ -46,7 +46,30 @@ void EXTI_vEnableEXTI(E_Line_t copy_E_Line, E_Port_t copy_E_Port, E_State_t copy
 	SET_BIT(EXTI->IMR, copy_E_Line);    // Set the Interrupt Not Masked
 
 	AFIO_vSetEXTIConfigration(copy_E_Line, copy_E_Port);
+
+	switch(copy_E_Line)
+	{
+		case LINE0      : EXTI_fCallBack[0] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+		case LINE1      : EXTI_fCallBack[1] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+		case LINE2      : EXTI_fCallBack[2] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+		case LINE3      : EXTI_fCallBack[3] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+		case LINE4      : EXTI_fCallBack[4] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+
+		case LINE5      : 
+		case LINE6      : 
+		case LINE7      : 
+		case LINE8      : 
+		case LINE9      : EXTI_fCallBack[5] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+
+		case LINE10     : // Make it or with 11 &12&13&14&15
+		case LINE11     : 
+		case LINE12     : 
+		case LINE13     : 
+		case LINE14     : 
+		case LINE15     : EXTI_fCallBack[6] = copyEXTI_fCallBack; break; // Set the Call Back ptr
+	}
 }
+
 
 
 
@@ -83,3 +106,74 @@ void EXTI_vSwTrigger(E_Line_t copy_E_Line)
 	SET_BIT(EXTI->SWIER, copy_E_Line);    // Set The Software Interrupt Flag
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+void EXTI0_IRQHandler(void)
+{
+	EXTI_fCallBack[0]();
+	SET_BIT(EXTI -> PR , LINE0);
+}
+
+
+
+
+
+void EXTI1_IRQHandler(void)
+{
+	EXTI_fCallBack[1]();
+	SET_BIT(EXTI -> PR , LINE1);
+}
+
+
+
+
+void EXTI2_IRQHandler(void)
+{
+	EXTI_fCallBack[2]();
+	SET_BIT(EXTI -> PR , LINE2);
+}
+
+
+
+void EXTI3_IRQHandler(void)
+{
+	EXTI_fCallBack[3]();
+	SET_BIT(EXTI -> PR , LINE3);
+}
+
+
+
+
+void EXTI4_IRQHandler(void)
+{
+	EXTI_fCallBack[4]();
+	SET_BIT(EXTI -> PR , LINE4);
+}
+
+
+
+
+
+void EXTI9_5_IRQHandler(void)
+{
+	EXTI_fCallBack[5]();
+}
+
+
+
+
+
+void EXTI15_10_IRQHandler(void)
+{
+	EXTI_fCallBack[6]();
+}
